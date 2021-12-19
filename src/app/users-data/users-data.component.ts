f6210f56b606060f04572989dfd2e9dcef6b9492
Marika-7 <raduga.dva@gmail.com>
2021-12-19 14:23:25 +0200

    advanced 2

---
+++ src/app/users-data/users-data.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-data',
+  templateUrl: './users-data.component.html',
+  styleUrls: ['./users-data.component.scss']
+})
+export class UsersDataComponent implements OnInit {
+  public isBtn = true;
+  public inputWrong1 = false;
+  public inputWrong2 = false;
+  public inputWrong3 = false;
+  public userIndex!: number;
+  public user = {
+    login: '',
+    password: '',
+    email: ''
+  };
+  // public usersArray: Array<Object> = [];
+  // public usersArray: Array<{login: string, password: string, email: string}> = [];
+  // ------------------
+  public usersArray = [
+    {
+      login: 'ivan',
+      password: '12345',
+      email: 'ii@gmail.com'
+    },
+    {
+      login: 'petro',
+      password: 'qwerty',
+      email: 'pp@gmail.com'
+    },
+    {
+      login: 'pavlo',
+      password: 'asdfgh',
+      email: 'pav@gmail.com'
+    },
+    {
+      login: 'marika',
+      password: '7777777',
+      email: 'mm@gmail.com'
+    },
+    {
+      login: 'raduga',
+      password: 'raduga',
+      email: 'ra@gmail.com'
+    },
+    {
+      login: 'dima',
+      password: '54321',
+      email: 'dd@gmail.com'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // * add new user into usersArray
+  addUser(): void {
+    if(!this.checkInputs()) {
+      return;
+    }
+    this.usersArray.push(this.createNewUser());
+    this.clearInputs();
+  }
+
+  // * check inputs
+  checkInputs(): boolean {
+    if(!this.user.login) {
+      this.inputWrong1 = true;
+      return false;
+    }
+    else {
+      this.inputWrong1 = false;
+    }
+    if(!this.user.password) {
+      this.inputWrong2 = true;
+      return false;
+    }
+    else {
+      this.inputWrong2 = false;
+    }
+    if(!this.user.email) {
+      this.inputWrong3 = true;
+      return false;
+    }
+    else {
+      this.inputWrong3 = false;
+    }
+    return true;
+  }
+
+  // * create new user with data from inputs
+  createNewUser() {
+    return {
+      login: this.user.login,
+      password: this.user.password,
+      email: this.user.email
+    };
+  }
+
+  // * clear input fields
+  clearInputs():void {
+    this.user.login = '';
+    this.user.password = '';
+    this.user.email = '';
+  }
+
+  // * delete user from usersArray
+  deleteUser(index: number): void {
+    this.usersArray.splice(index, 1);
+  }
+
+  // * get data of user for editing
+  editUser(index: number): void {
+    this.isBtn = false;
+    this.user.login = this.usersArray[index].login;
+    this.user.password = this.usersArray[index].password;
+    this.user.email = this.usersArray[index].email;
+    this.userIndex = index;
+  }
+
+  // * save edit data of user
+  saveEditUser(): void {
+    if(!this.checkInputs()) {
+      return;
+    }
+    this.usersArray[this.userIndex] = this.createNewUser();
+    this.isBtn = true;
+    this.clearInputs();
+  }
+
+}
